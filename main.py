5bc12b9be81cd4ce4a4d0a5688aa7c8e3b389cdf
Aleksandr Buganov <rivares89@gmail.com>
2023-10-04 23:29:47 +0300

    fix

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from src.utils import get_data, get_filtr_data, get_last_values, get_format_data
 
 def main():
-    data = get_data()
-    data = get_filtr_data(data)
-    data = get_last_values(data)
-    format_data  = get_format_data(data)
-    print(format_data)
+    data = get_data() # получает данные
+    data = get_filtr_data(data)  # фильтр по ключу state
+    data = get_last_values(data) # берет последние 5 операций
+    format_data  = get_format_data(data) # формирует отчет
     for item in format_data:
         print(item)
 
