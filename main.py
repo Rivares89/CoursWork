2c41ee04f66f764b7de5493a8df6fbb8d12aa6a3
Aleksandr Buganov <rivares89@gmail.com>
2023-10-04 23:06:44 +0300

    make tests, gitignore, readme, requarements

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-from utils import get_data, get_filtr_data, get_last_values, get_format_data
+from src.utils import get_data, get_filtr_data, get_last_values, get_format_data
 
 def main():
     data = get_data()
     data = get_filtr_data(data)
     data = get_last_values(data)
     format_data  = get_format_data(data)
+    print(format_data)
     for item in format_data:
         print(item)
-    # for count in data:
-    #     print (count['date'])
-    # print (data[1])
-
 
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -3,20 +3,24 @@ import json
 from datetime import datetime
 
 def get_data():
+    '''Фнукция берет данные из файла operations.json'''
     with open ('operations.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
 def get_filtr_data(data):
+    '''Данные фильтруются по ключу state, остаются только выполненные операции'''
     data = [x for x in data if 'state' in x and x.get('state') == 'EXECUTED']
     return data
 
 def get_last_values(data):
+    '''Функция сортирует операции по дате и отбирает последние 5 операций '''
     data = sorted(data, key=lambda x: x["date"], reverse=True)
     data = data[:5]
     return data
 
 def get_format_data(data):
+    '''Функция собирает информацию для рапорта: дата, тип, с какого счета или карты, на какой счет, сумма и валюта'''
     format_data = []
     for count in data:
         date = datetime.strptime(count['date'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%d.%m.%Y')
@@ -24,10 +28,11 @@ def get_format_data(data):
         from_ = from_to(count, 'from')
         to_ = from_to(count, 'to')
         amount = f"{count['operationAmount']['amount']} {count['operationAmount']['currency']['name']}"
-        format_data.append(f'{date} {description}\n{from_} -> {to_}\n{amount}')
+        format_data.append(f'{date} {description}\n{from_} -> {to_}\n{amount}\n')
     return format_data
 
 def from_to(count, from_or_to):
+    '''Фунция осуществляет кодирование личных данных, заменяет часть цифр на *'''
     if count.get(from_or_to):
         from_list = count.get(from_or_to).split(" ")
         if from_list[0] == "Счет":
